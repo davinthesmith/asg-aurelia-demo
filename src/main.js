4b2f64c2d3b6bbb840365b78e576e8864e5b6432
Davin Smith <dsmith@prodova.net>
2018-02-15 14:08:17 -0600

    Cleared environment references since it's not helpful for the intro

--- src/main.js
+++ src/main.js
@@ -1,17 +1,8 @@
-import environment from './environment';
 import { PLATFORM } from 'aurelia-pal';
 import 'bulma/css/bulma.css';
 
 export function configure(aurelia) {
   aurelia.use.standardConfiguration();
-
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
-
-  if (environment.testing) {
-    aurelia.use.plugin(PLATFORM.moduleName('aurelia-testing'));
-  }
 
   aurelia.start().then(() => aurelia.setRoot(PLATFORM.moduleName('app')));
 }
